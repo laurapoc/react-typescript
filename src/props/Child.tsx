11ba51e933d36a7c6de1427aaebf5a0d2751c33f
laurapoc <laura.pociuniene@gmail.com>
2021-09-09 19:53:09 +0300

    first commit repeating react-typescript basics

---
+++ src/props/Child.tsx
@@ -0,0 +1,15 @@
+interface ChildProps {
+  color: string;
+}
+
+export const Child = ({ color }: ChildProps) => {
+  return (
+    <div>
+      <h1>{color}</h1>
+    </div>
+  );
+};
+
+export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
+  return <div>{color}</div>;
+};
