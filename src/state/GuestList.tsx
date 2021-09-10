ff322b840f74d36381b92bb2e9dd2283c399faa2
laurapoc <laura.pociuniene@gmail.com>
2021-09-10 11:25:39 +0300

    add onClick funtion to add guest list on button click

--- src/state/GuestList.tsx
+++ src/state/GuestList.tsx
@@ -2,12 +2,18 @@ import { useState } from "react";
 
 const GuestList: React.FC = () => {
   const [name, setName] = useState("");
+  const [guests, setGuests] = useState<Array<string>>([]);
+
+  const onClick = () => {
+    setName("");
+    setGuests([...guests, name]);
+  };
 
   return (
     <div>
       <h1>Guest List:</h1>
       <input value={name} onChange={(e) => setName(e.target.value)} />
-      <button>Add Guest</button>
+      <button onClick={onClick}>Add Guest</button>
     </div>
   );
 };
